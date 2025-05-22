3d6ce7c00b672ea684e95a4664eacffe9bac329f
Carolista <cjones.renaissancewoman@gmail.com>
2025-05-22 18:27:17 -0400

    removed solution to create starter code

--- helpers.js
+++ helpers.js
@@ -1,101 +1,26 @@
-const fs = require('fs');
-const input = require('readline-sync');
+// TODO: import fs and readline-sync
+
 
 // TODO: Transform JSON to JS
-function getJSFromJSON(filePath) {
-	const jsonData = fs.readFileSync(filePath, 'utf-8');
-	return JSON.parse(jsonData);
-}
+
 
 // TODO: Sort results by last name
-function sortByLastName(data) {
-    data.sort((a, b) => a.lastName - b.lastName);
-    // No return - it mutates the array
-}
+
 
 // TODO: Ask user for type of search & validate response
-function getSearchType() {
-	let options = `  0 - Search by name
-  1 - Search by first letter of last name
-  2 - Search by area code
-  3 - Display all
-`;
-    let question = `
-What kind of search would you like? 
-${options}`;
-	let searchType = input.question(question);
-	// Validate
-	while (searchType < 0 || searchType > 3 || isNaN(searchType)) {
-		searchType = input.question(`
-Please enter a valid number. ${question} 
-${options}`);
-	}
-	return searchType;
-}
+
 
 // TODO: Filter listings by name (check both first and last names for keyword)
-function searchByName(data) {
-	// Ask user for all or part of a name
-	let searchTerm = input.question('\nPlease enter first OR last name: \n');
-	// Validate for numbers or spaces
-	while (searchTerm.includes(' ') || !isNaN(searchTerm)) {
-		searchTerm = input.question(
-			'\nNo numbers or spaces — please enter all or part of a first OR last name: \n'
-		);
-	}
-	// Filter using search term
-	return data.filter(doctor =>
-		(doctor.firstName + doctor.lastName).toLowerCase().includes(searchTerm.toLowerCase())
-	);
-}
+
 
 // TODO: Filter listings by first letter of last name
-function searchByFirstLetter(data) {
-	// Ask user for a letter
-	let letter = input.question("\nWhich letter of the alphabet would you like to use to search last names?\n");
-	// Validate user response to ensure it is just one letter
-	while (!isNaN(letter) || letter.length > 1) {
-		letter = input.question("\nPlease enter a single letter of the alphabet:\n");
-	}
-    // Filter using letter
-	return data.filter(doctor => letter.toUpperCase() === doctor.lastName[0]);
-}
+
 
 // TODO: Filter listings by area code
-function searchByAreaCode(data) {
-	// Ask user for an area code
-	let areaCode = input.question("\nWhich area code would you like to search? Choices are 314, 618, and 636:\n");
-	// Validate user response
-	while (!["314", "618", "636"].includes(areaCode)) {
-		areaCode = input.question(`\nPlease enter a valid area code. Choices are 314, 618, and 636:\n`);
-	}
-	// Filter using area code
-	return data.filter(doctor => doctor.phoneNumber.startsWith(areaCode));
-}
+
 
 // TODO: Format and print results
-function displaySearchResults(results) {
-    // Handle cases where results array has no records
-	if (results.length === 0) {
-		console.log("\nSorry, no results found. \n");
-	} else {
-		console.log("\nRESULTS *******************************\n");
-		console.log(`${results.length} results found:\n`)
-		results.forEach(doctor => {
-			console.log(`Dr. ${doctor.firstName} ${doctor.lastName}`);
-			console.log(`${doctor.phoneNumber}\n`);
-        });
-		console.log("***************************************");
-	}
-	// no return statement
-}
 
-module.exports = {
-	getJSFromJSON,
-    sortByLastName,
-    getSearchType,
-	searchByName,
-	searchByFirstLetter,
-	searchByAreaCode,
-	displaySearchResults,
-};
+
+// TODO: Export everything!
+
--- index.js
+++ index.js
@@ -1,63 +1,22 @@
 /**** IMPORTS ****/
 
 // TODO: import readline-sync library & helper functions
-const input = require('readline-sync');
-const {
-	getJSFromJSON,
-    getSearchType,
-	searchByName,
-	searchByFirstLetter,
-	searchByAreaCode,
-	displaySearchResults,
-    sortByLastName,
-} = require('./helpers.js');
+
 
 /**** DATA ****/
 
 // TODO: Transform data
-const doctorData = getJSFromJSON('./doctors.json');
+
 
 // TODO: Sort data by last name
-sortByLastName(doctorData);
+
 
 // TODO: Collect 3 search functions in an array (corresponding to types)
-const searchFunctions = [searchByName, searchByFirstLetter, searchByAreaCode];
+
 
 /**** MAIN FUNCTION ****/
 
 // TODO: Put everything together in a single function
-function runProgram() {
-	
-    // Display intro to user
-	console.log("Welcome to the Directory of Physicians for the St. Louis Metropolitan Area!\n");
-	let keepSearching = true;
 
-	// Start loop
-	while (keepSearching) {
-		// Find out which search type the user wants
-		let index = getSearchType();
-		let results; // declare only
-        
-		// Conduct corresponding search
-		if (index === '3') {
-			// If index 3, just get all listings
-			results = doctorData.slice(0);
-		} else {
-			// If index 0-2, use corresponding search function
-			results = searchFunctions[index](doctorData);
-		}
-
-		// Display results
-		displaySearchResults(results);
-
-		// Ask if user wants to search again
-		if (input.question("\nAnother search? (Y/N) ").toUpperCase() !== "Y") {
-			keepSearching = false;
-		}		
-	}
-	// End program
-	console.log("\nThank you for using our directory!\n")
-}
 
 // TODO: Call main function so that program will run
-runProgram();
